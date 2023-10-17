d674ba391e79aa7b59541ba45130f1ecc36bbfb3
Alexey Polyakov <aleksey.polyakov@ffintech.com>
2023-10-17 19:33:05 +0300

    moved css to _app

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,3 @@
-import '@/styles/index.css';
-
 function Layout({ children }) {
     return (
         <div>
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,7 @@
 import Layout from '@/components/Layout/Layout.jsx';
 
+import '@/styles/index.css';
+
 export default function MyApp({ Component, pageProps }) {
     return (
         <Layout>
